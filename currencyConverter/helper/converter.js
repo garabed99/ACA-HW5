c20d507ca670f3547154ba9576529ea9815c8f69
Garabed Baghsarian <garabed.baghsarian@gmail.com>
2021-12-06 18:09:17 +0400

    commiting currency converter 1.0

---
+++ currencyConverter/helper/converter.js
@@ -0,0 +1,7 @@
+export function toAMD(amount) {
+  return amount * 491;
+}
+
+export function toUSD(amount) {
+  return amount * 0.002;
+}
---
+++ currencyConverter/helper/updater.js
@@ -0,0 +1,7 @@
+export function updateFrom() {
+
+}
+
+export function updateTo() {
+    
+}
